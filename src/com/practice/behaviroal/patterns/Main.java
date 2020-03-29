a2114ac0ed421f43844f9d7f807d0dc8c9522580
Ubaid Rana <ubaidrana45@gmail.com>
2020-03-29 13:33:59 +0200

    Memento Pattern

--- src/com/practice/behaviroal/patterns/Main.java
+++ src/com/practice/behaviroal/patterns/Main.java
@@ -1,8 +1,25 @@
 package com.practice.behaviroal.patterns;
 
+import com.practice.behaviroal.patterns.memento.Editor;
+import com.practice.behaviroal.patterns.memento.History;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
